0ef299e8305bc92d1758aacc153b06e99842a91a
dev0Guy <97923827+dev0Guy@users.noreply.github.com>
2025-01-17 17:48:09 +0200

    add docs

--- src/engine/gui/base.rs
+++ src/engine/gui/base.rs
@@ -1,7 +1,25 @@
 use crate::engine::board::board::Board;
+use crate::engine::board::pieces::Side;
 
+/// A trait representing the graphical user interface (GUI) for any board game.
+///
+/// This trait defines the basic operations required for rendering the game state
+/// and handling user interactions. Implementers of this trait can create custom GUIs
+/// for different platforms or rendering backends.
+///
+/// ## Associated Type
+/// - `T`: Represents the type of action or event that the GUI processes and returns.
+///
 pub trait GUI<T>{
-    fn render(&mut self, game: &Board);
+    /// Renders the current game state on the GUI.
+    ///
+    /// This method takes a reference to the [`Board`] and displays it in the GUI.
+    ///
+    /// ## Parameters
+    /// - `game`: A reference to the current game state, represented by the [`Board`].
+    /// - `active_side`: current side turn.
+    fn render(&mut self, game: &Board, active_side: Side);
 
     fn wait_and_process_event(&mut self) -> T;
+
 }
