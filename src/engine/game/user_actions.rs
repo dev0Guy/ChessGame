684c052c328c20c4f063f75586f76594b4c45f02
dev0Guy <97923827+dev0Guy@users.noreply.github.com>
2025-01-17 17:49:15 +0200

    update user action

---
+++ src/engine/game/user_actions.rs
@@ -0,0 +1,25 @@
+use crate::engine::board::board;
+use crate::engine::board::location::Location;
+
+#[derive(Debug)]
+pub struct MoveAction {
+    pub from: Location,
+    pub to: Location,
+}
+
+#[derive(Debug)]
+pub enum UserAction<MoveAction> {
+    OfferDraw,
+    AcceptDraw,
+    Resign,
+    Move(MoveAction),
+    ShowMoveOption(Location),
+}
+
+pub type Action = UserAction<MoveAction>;
+
+impl MoveAction {
+    pub fn new(from: Location, to: Location) -> Self{
+        Self{from, to}
+    }
+}
