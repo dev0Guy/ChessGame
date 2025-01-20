9ac9955e5ef02c9b0cd9da258e7867e24070c5ee
dev0Guy <97923827+dev0Guy@users.noreply.github.com>
2025-01-20 21:31:21 +0200

    fix chess check problem

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,9 @@
-mod engine;
-mod game;
-
-use engine::game::base::Game;
-use engine::gui::{cmd::CommandPromptGUI};
-
-// TODO:
-// [V] Document every thing
-// [V] Create Tests
-// [V] Show in console all of the possible moves
-// [V] Add King
-// [V] Add knight moves
-// [V] Pin Piece
-// [ ] Castle
-// [V] Add validation action for each piece
-// [ ] Add memory for each piece on each location all possible moves
-// [ ] Combine precalculate with validate function
+mod bitboard;
 
 
 fn main() {
-    let gui= CommandPromptGUI::new();
-    let mut game = Game::new(gui);
-    game.start();
+    let x = bitboard::BitBoard::new(0xff);
+    println!("{:?}", x);
+
+    println!("Hello, world!");
 }
